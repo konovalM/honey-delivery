8d24cf35a2927c802d0d54dada812f2d8520fb34
Maxim Konovalenko <konovalenko.ii4elka@mail.ru>
2025-06-05 03:34:48 +0300

    fix base url

--- frontend/src/shared/api/http.ts
+++ frontend/src/shared/api/http.ts
@@ -1,9 +1,9 @@
 import { LOCAL_STORAGE_ACCESS_TOKEN } from "@shared/const/const";
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_APP_URL ?? 'https://honey.local/';
+const BASE_URL = import.meta.env.VITE_APP_URL ?? 'https://honey.local/';
 
-const API_URL = import.meta.env.VITE_APP_URL + 'api';
+const API_URL = BASE_URL + 'api';
 
 const http = axios.create({
     baseURL: API_URL,
